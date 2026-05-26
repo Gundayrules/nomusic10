33b04457844b99197029f772331a3ddb6ed859d7
Atul tiwari <162774395+Gundayrules@users.noreply.github.com>
2026-05-26 13:22:57 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -67,8 +67,8 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url="https://t.me/BRANDEDKING8"),
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/BRANDED_WORLD"),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url="https://t.me/Lemeethinkk"),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/atultechcare9"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -85,8 +85,8 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url="https://t.me/BRANDEDKING8"),
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/BRANDED_WORLD"),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", url="https://t.me/Lemeethinkk"),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url="https://t.me/atultechcare9"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
